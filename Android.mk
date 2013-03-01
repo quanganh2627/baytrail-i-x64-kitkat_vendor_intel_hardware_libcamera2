9a76eefe7207399a58f67571c615eb7bc53c6c60
buildslave <buildslave@buildbot.tl.intel.com>
2013-03-01 11:07:20 -0800

    Automatically revert commits from previous weekly release
    Change-Id: Ib93da99adb57946928f915e6c48c3e2e6c6b41c4
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- Android.mk
+++ Android.mk
@@ -128,7 +128,10 @@ endif
 
 ifeq ($(REF_DEVICE_NAME),mfld_gi)
 LOCAL_CFLAGS += -DMFLD_GI
-else ifneq (,$(findstring $(REF_DEVICE_NAME),victoriabay redhookbay))
+
+else ifneq (,$(findstring $(REF_DEVICE_NAME),mfld_dv10 redridge))
+LOCAL_CFLAGS += -DMFLD_DV10
+else ifneq (,$(findstring $(REF_DEVICE_NAME),victoriabay ctp_pr1 ctp_nomodem))
 LOCAL_CFLAGS += -DCLVT
 else ifeq ($(REF_DEVICE_NAME),mrfl_vp)
 LOCAL_CFLAGS += -DMRFL_VP
