941dd96b2d6b690f89c489ca3b277205fd812ead
Teemu Tuominen <teemux.tuominen@intel.com>
2013-03-27 11:35:56 +0200

    [PORT FROM MAIN] libcamera2: cleanup preview state- and capture sequence-semantics
    BZ: 73975
    
    ControlThread's internal state has no longer reflected the real capture
    state towards the public HAL API. This is due that ControlThread does not
    transit its state from preview to capture when running in continuous-mode
    and also due that the state transitions to capture always happen within
    asynchronous message handler.
    
    This change introduces ControlThread::mStillCaptureInProgress to simplify
    the state needed for acting coherently with HAL API on sequences starting
    from takePicture(). Semantic of previewEnabled() during
    mStillCaptuerInProgress is true is separated from PreviewThread states.
    
    Drops preview-keep-alive parameter extension and changes the naming
    conventions of postview rendering to not to confuse with the removed
    extension.
    
    ControlThread is made responsible of hiding the preview and deciding
    whether to pass the postview frames to display in different capture
    modes. Postview frames get displayed regardless if preview state is
    set to hidden.
    
    PreviewThread::postview() now supports hide-request flag to be able
    to hide the following preview stream after the postview is handled.
    By default, hiding is requested with single capture done in continuous
    mode. When postview of correct format is send to PreviewThread it gets
    rendered also in hidden state.
    
    Change-Id: I71c40d810c4bf4ec39f6891975c7a5fd41b817ed
    Orig-Change-Id: Ie8ff7115b312ff1590e7bbb4a45c3598784f3e35
    Signed-off-by: Teemu Tuominen <teemux.tuominen@intel.com>
    Reviewed-on: http://android.intel.com:8080/100220
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    Reviewed-on: http://android.intel.com:8080/101751

--- ControlThread.h
+++ ControlThread.h
@@ -213,6 +213,7 @@ private:
 
     struct MessageSetParameters {
         char* params;
+        bool stopPreviewRequest;
     };
 
     struct MessageCommand{
@@ -680,6 +681,13 @@ private:
     bool mEnableFocusCbAtStart;     /* for internal control of focus cb's in continuous-mode */
     bool mEnableFocusMoveCbAtStart; /* for internal control of focus-move cb's in continuous-mode */
     bool mFirstPreviewStart;        /* indicator of first preview start for L2P pnp optimizations */
+
+    bool mStillCaptureInProgress;   /*!< indicates ongoing capture sequence for Camera_HAL API.
+                                         note: threadsafe to use only in Camera_HAL calling context
+                                               or synchronous messages from them
+                                         note: is set to true when takePicture() is called in other
+                                               than video recording state. Remains true until following
+                                               call to startPreview() or cancelPicture(). */
 }; // class ControlThread
 
 }; // namespace android
