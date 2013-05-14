31dec841e59c4302213c7e307f5176a73da451fb
Jin Wei <wei.a.jin@intel.com>
2013-05-14 13:27:46 +0800

    [PDK MR2 Temp] fix pdk build error
    BZ: 117199
    
    Revert "libcamera2: use -Werror for R&D builds"
    This reverts commit c4acb5daacf8eca7bc7f80ca5113ca4d5f94f539.
    
    Change-Id: I64eab706aee3acd6cbd9a905e543ad215f7fcc43

--- Android.mk
+++ Android.mk
@@ -158,7 +158,7 @@ endif
 
 # enable R&D features only in R&D builds
 ifneq ($(filter userdebug eng tests, $(TARGET_BUILD_VARIANT)),)
-LOCAL_CFLAGS += -DLIBCAMERA_RD_FEATURES -Wunused-variable -Werror
+LOCAL_CFLAGS += -DLIBCAMERA_RD_FEATURES -Wunused-variable
 endif
 
 # The camera.<TARGET_DEVICE>.so will be built for each platform
