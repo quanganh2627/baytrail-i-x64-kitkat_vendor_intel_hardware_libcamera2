7d43e15d6cb8be39cbfc57f74daeec8fad9124c8
Lassi Väätämöinen <lassix.vtminen@intel.com>
2013-09-20 14:41:16 +0300

    [PORT FROM MAIN] libcamera2: [REVERTME] drop corrupt AE statistics
    BZ: 139972
    
    Implements a heuristic algorithm for dropping ISP statistics,
    depending on the variation between previous and current statistics.
    The threshold for dropping the statistics is adjustable, based on
    perceived performance. Also the statistics should not be dropped
    during the (pre-)flash sequence.
    
    This patch cleans up the previous implementation, as well as
    fixes a regression in the image exposure when using flash.
    The regression was because of an extra 3A iteration in the pre-flash
    sequence due to additional applyPreFlashProcess() call.
    
    NOTE: Revert once BZ 119181 is resolved.
    
    Based on change I1c7d354bb0adbb352d66d0b67a9c7b80d5f4ee81
    
    Change-Id: Ic323eabcc8a546c06f45a81876109cde16d0f352
    Orig-Change-Id: I34244f863d94575fdcadf0ff7e6966c19211d710
    Signed-off-by: Lassi Väätämöinen <lassix.vtminen@intel.com>
    Reviewed-on: http://android.intel.com:8080/136759
    Reviewed-by: Hu, Gang A <gang.a.hu@intel.com>
    Tested-by: Hu, Gang A <gang.a.hu@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- AtomISP.h
+++ AtomISP.h
@@ -409,6 +409,9 @@ private:
     status_t applySensorFlip(void);
     void fetchIspVersions();
 
+    // TODO: Remove once BZ #119181 gets fixed by the firmware team!!
+    int detectCorruptStatistics(struct atomisp_3a_statistics *statistics);
+
 private:
     // AtomIspObserver
     IObserverSubject* observerSubjectByType(ObserverType t);
@@ -579,9 +582,10 @@ private:
     bool mHighSpeedEnabled;
 
     // Sensor helper fields
-    Vector <v4l2_fmtdesc>    mSensorSupportedFormats;     /*!> List of V4L2 pixel format supported by the sensor */
+    Vector <v4l2_fmtdesc>    mSensorSupportedFormats;     /*!< List of V4L2 pixel format supported by the sensor */
     int                      mRawBayerFormat;
-
+    bool mFlashIsOn;                                    //!< Used in corrupt statistics detection to avoid dropping
+                                                        // stats when flash is used */
 }; // class AtomISP
 
 }; // namespace android
