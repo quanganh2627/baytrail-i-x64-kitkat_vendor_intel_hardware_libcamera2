d4a425c898ff34d8a3cab24d25d1ab6c0fd72750
Yu CUI <yux.cui@intel.com>
2013-10-29 09:53:25 +0800

    [PORT FROM R42B]libcamera2: set the default snapshot size to 1080p
    BZ: 140905
    
    Root cause:
    Barcodes scanner doesn't care about the size of snapshot,
    it use the default value that hal set.
    And it will set the preview size which is matched with
    screen size.
    Our sensor can only output one ratio, so to choose
    a ratio to match the screen is the best way to avoid stretch.
    
    Change-Id: I4eb4803432006e7e150c3f59d531660e0bc4155c
    Orig-Change-Id: I3e4361a30ac5e30b7757c5940f7fbc54ed4f6a15
    Signed-off-by: Yu CUI <yux.cui@intel.com>
    Signed-off-by: Yu CUI <yux.cui@intel.com>

--- AtomCommon.h
+++ AtomCommon.h
@@ -266,7 +266,7 @@ const struct AtomFormatBridge* getAtomFormatBridge(unsigned int fourcc);
  * @param second the second element of the pair, the memory need to be released by caller
  * @param delim delimiter of the pair
  */
-static int parsePair(char *str, char **first, char **second, const char *delim)
+static int parsePair(const char *str, char **first, char **second, const char *delim)
 {
     if(str == NULL)
         return -1;
@@ -280,6 +280,28 @@ static int parsePair(char *str, char **first, char **second, const char *delim)
     strncpy(*first, str, strlen(str) - strlen(index));
     strncpy(*second, str + strlen(str) - strlen(index) + 1, strlen(index) - 1);
     return 0;
+}
+
+/** If forgetting to free, parsePair may have memleak.
+ *  ParsePairToInt avoids this memleak.
+ */
+static int parsePairToInt(const char *str, int *first, int *second, const char *delim)
+{
+    char *a = NULL;
+    char *b = NULL;
+    int ret = parsePair(str, &a, &b, delim);
+
+    if (ret == 0 && a != NULL && b != NULL) {
+        *first = atoi(a);
+        *second = atoi(b);
+    }
+
+    if(a != NULL)
+        free(a);
+    if(b != NULL)
+        free(b);
+
+    return ret;
 }
 
 /**
