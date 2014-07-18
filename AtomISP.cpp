09c4b69ce02d3087a827d241264553c060b60b3e
Kun, Jiang <kunx.jiang@intel.com>
2014-07-18 13:54:46 +0800

    [MRD8][CTSVerifier]OV2722's video resolution QCIF/CIF/QVGA/VGA need to be re-tuned.
    BZ: borqs 0061339
    
    [MRD8][CTSVerifier]OV2722's video resolution QCIF/CIF/QVGA/VGA need to be re-tuned. FPS is
     low(about 15), FOV is small.
    rootcause:
    Follow platform's handle for case raw sensor can't provide
    small resolutions(whose FOV is okay) for video recording.
    
    Borig-Change-Id: Idd6959e5fc5b0db7d07f9a5328292f072127dca2
    Reviewed-on: http://192.168.1.79/gerrit/7851
    Reviewed-by: b444 <b444@borqs.com>
    
    Change-Id: I5a9cfca9003879a7e9ce757bb519819d44a9edb6
    Signed-off-by: Kun, Jiang <kunx.jiang@intel.com>

--- AtomISP.cpp
+++ AtomISP.cpp
@@ -2547,6 +2547,19 @@ bool AtomISP::applyISPLimitations(CameraParameters *params,
 			
 		}
 
+		const char manUsensorBNameOV2722[] = "ov2722";
+		if (strncmp(mSensorHW.getSensorName(), manUsensorBNameOV2722, sizeof(manUsensorBNameOV2722) - 1) == 0) {
+			if (((previewWidth == 176)&& (previewHeight == 144)) || ((previewWidth == 352)&& (previewHeight == 288))) {
+				LOGD("QCIF/CIF recording change preview size to 1320x1080");
+				params->setPreviewSize(1320,1080);
+			}
+
+			if(((previewWidth == 320)&& (previewHeight == 240)) || ((previewWidth == 640)&& (previewHeight == 480))) {
+				LOGD("QVGA/VGA recording change preview size to 1440x1080");
+				params->setPreviewSize(1440,1080);
+			}
+	}
+
         //Workaround 2, detail refer to the function description
         params->getPreviewSize(&previewWidth, &previewHeight);
         params->getVideoSize(&videoWidth, &videoHeight);
