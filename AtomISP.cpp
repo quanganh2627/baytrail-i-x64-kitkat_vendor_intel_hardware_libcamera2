28e7cf6feb003c2f819da7e1eb1b6a75347f30ac
Kun, Jiang <kunx.jiang@intel.com>
2014-07-18 14:07:01 +0800

    MRD8's OV5693, MRD7's GC0339 resolution set as LOW/QCIF/CIF, FOV is small when  recording.
    BZ: borqs 0061386
    
    Camera Video test,when camera set as
    camera0,resolution set as LOW/QCIF/CIF,FOV is small when
    recording. borqsbtx: 0061386 rootcause: Tuning small
    resolution of video, for MRD8's OV5693, MRD7's GC0339,
    following platform's handle routine.
    
    Borig-Change-Id: I34e68414f84d012200d68e50390e03d6788cac53
    Reviewed-on: http://192.168.1.79/gerrit/7871
    Reviewed-by: b444 <b444@borqs.com>
    
    Change-Id: I10e51512d6efc3ed1c80c182a94466921881fa5a
    Signed-off-by: Kun, Jiang <kunx.jiang@intel.com>

--- AtomISP.cpp
+++ AtomISP.cpp
@@ -2547,18 +2547,39 @@ bool AtomISP::applyISPLimitations(CameraParameters *params,
 			
 		}
 
-		const char manUsensorBNameOV2722[] = "ov2722";
-		if (strncmp(mSensorHW.getSensorName(), manUsensorBNameOV2722, sizeof(manUsensorBNameOV2722) - 1) == 0) {
-			if (((previewWidth == 176)&& (previewHeight == 144)) || ((previewWidth == 352)&& (previewHeight == 288))) {
-				LOGD("QCIF/CIF recording change preview size to 1320x1080");
-				params->setPreviewSize(1320,1080);
-			}
+        const char manUsensorBNameGC0339[] = "gc0339";
+        if (strncmp(mSensorHW.getSensorName(), manUsensorBNameGC0339, sizeof(manUsensorBNameGC0339) - 1) == 0) {
+            if (((previewWidth == 176)&& (previewHeight == 144)) || ((previewWidth == 352)&& (previewHeight == 288))) {
+                LOGD("QCIF/CIF recording change preview size to 528x432");
+                params->setPreviewSize(528,432);
+            }
+        }
 
-			if(((previewWidth == 320)&& (previewHeight == 240)) || ((previewWidth == 640)&& (previewHeight == 480))) {
-				LOGD("QVGA/VGA recording change preview size to 1440x1080");
-				params->setPreviewSize(1440,1080);
-			}
+	const char manUsensorBNameOV2722[] = "ov2722";
+	if (strncmp(mSensorHW.getSensorName(), manUsensorBNameOV2722, sizeof(manUsensorBNameOV2722) - 1) == 0) {
+		if (((previewWidth == 176)&& (previewHeight == 144)) || ((previewWidth == 352)&& (previewHeight == 288))) {
+			LOGD("QCIF/CIF recording change preview size to 1320x1080");
+			params->setPreviewSize(1320,1080);
+		}
+
+		if(((previewWidth == 320)&& (previewHeight == 240)) || ((previewWidth == 640)&& (previewHeight == 480))) {
+			LOGD("QVGA/VGA recording change preview size to 1440x1080");
+			params->setPreviewSize(1440,1080);
+		}
 	}
+
+        const char manUsensorBNameOV5693[] = "ov5693";
+        if (strncmp(mSensorHW.getSensorName(), manUsensorBNameOV5693, sizeof(manUsensorBNameOV5693) - 1) == 0) {
+            if (((previewWidth == 176)&& (previewHeight == 144)) || ((previewWidth == 352)&& (previewHeight == 288))) {
+                LOGD("QCIF/CIF recording change preview size to 1320x1080");
+                params->setPreviewSize(1320,1080);
+            }
+
+            if(((previewWidth == 320)&& (previewHeight == 240)) || ((previewWidth == 640)&& (previewHeight == 480))) {
+                LOGD("QVGA/VGA recording change preview size to 1440x1080");
+                params->setPreviewSize(1440,1080);
+            }
+        }
 
         //Workaround 2, detail refer to the function description
         params->getPreviewSize(&previewWidth, &previewHeight);
